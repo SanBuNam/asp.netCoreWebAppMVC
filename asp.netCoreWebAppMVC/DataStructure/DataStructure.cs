5afb6c568e71a65ae772e7b579a4ecec1a5424a5
David Lee <stoicSanbunam@gmail.com>
2021-09-01 20:47:43 -0700

    data structure folder

---
+++ asp.netCoreWebAppMVC/DataStructure/DataStructure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace asp.netCoreWebAppMVC.DataStructure
+{
+    public class DataStructure
+    {
+        public static void Main()
+        {
+            Stack myStack = new Stack();
+            myStack.Push("");
+
+        }
+    }
+}
