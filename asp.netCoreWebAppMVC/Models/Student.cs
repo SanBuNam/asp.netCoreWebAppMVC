de39f9efe16e04ab1dd768a27dae1cf9b8afbc1d
David Lee <stoicSanbunam@gmail.com>
2021-09-02 19:26:02 -0700

    Models in .net Core MVC

---
+++ asp.netCoreWebAppMVC/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.netCoreWebAppMVC.Models
+{
+    public class Student
+    {
+        public string Gender { get; set; }
+        public string Section { get; set; }
+        public string Branch { get; set; }
+        public string Name { get; set; }
+        public int StudentID { get; set; }
+    }
+}
---
+++ asp.netCoreWebAppMVC/Models/TestStudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.netCoreWebAppMVC.Models
+{
+    public class TestStudentRepository : IStudentRepository
+    {
+        public List<Student> DataSource()
+        {
+            return new List<Student>()
+            {
+                new Student() {StudentID=101, Name = "James", Branch = "CSE", Section = "A", Gender = "Male"},
+                new Student() {StudentID=102, Name = "James2", Branch = "CSE2", Section = "A2", Gender = "Male"},
+                new Student() {StudentID=103, Name = "James3", Branch = "CSE3", Section = "A3", Gender = "Male"},
+                new Student() {StudentID=104, Name = "James4", Branch = "CSE4", Section = "A4", Gender = "Male"},
+                new Student() {StudentID=105, Name = "James5", Branch = "CSE5", Section = "A5", Gender = "Male"}
+            };
+        }
+
+        public Student GetStudentById(int StudentId)
+        {
+            return DataSource().FirstOrDefault(e => e.StudentID == StudentId);
+        }
+    }
+}
