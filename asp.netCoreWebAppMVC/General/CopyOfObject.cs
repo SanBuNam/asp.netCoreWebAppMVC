9a5f14147cee728bd5f41ad00bda4ea7f7afa992
David Lee <david.lee@amwestfunding.com>
2021-09-17 17:02:17 -0700

    shallow copy with MemberwiseClone

---
+++ asp.netCoreWebAppMVC/General/CopyOfObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.netCoreWebAppMVC.General
+{
+    // Object.MemberwiseClone() method can be used to create a shallow copy of the current object.
+    public class CopyObjectMemberwiseClone
+    {
+        public string str;
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+
+    public class MemberwiseCloneExample
+    {
+        public static void RunMain()
+        {
+            CopyObjectMemberwiseClone obj = new CopyObjectMemberwiseClone();
+            obj.str = "Hello!";
+
+            CopyObjectMemberwiseClone copy = (CopyObjectMemberwiseClone)obj.Clone();
+            Console.WriteLine(copy.str);
+        }
+    }
+
+    
+}
