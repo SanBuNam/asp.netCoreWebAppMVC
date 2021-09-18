7a8883fda1e298eefc926cacf726374bcb7a624c
David Lee <stoicSanbunam@gmail.com>
2021-09-18 08:47:06 -0700

    algo

---
+++ asp.netCoreWebAppMVC/algorithm/ReverseString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.netCoreWebAppMVC.Algorithm
+{
+    public class ReverseString
+    {
+        public static string ReverseStr(string str)
+        {
+            var result = "";
+
+            for (var i = str.Length -1; i >= 0; i--) {
+                result += result;
+            }
+
+            return result;
+        }
+    }
+}
