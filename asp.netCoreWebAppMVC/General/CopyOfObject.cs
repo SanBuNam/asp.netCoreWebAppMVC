3d5c3bfa26e98a56fe420a4a39a60d8ed0820e0d
David Lee <david.lee@amwestfunding.com>
2021-09-17 17:02:51 -0700

    Deep copy with constructor

--- asp.netCoreWebAppMVC/General/CopyOfObject.cs
+++ asp.netCoreWebAppMVC/General/CopyOfObject.cs
@@ -27,5 +27,24 @@ namespace asp.netCoreWebAppMVC.General
         }
     }
 
-    
+    // Copy Constructor takes another instance of the same class and defines the complier's actions when copying the object.
+    // The copy constructor implementation should perform deep copy for any referenced objects in the class by creating new objects and copying the immutable type's values.
+    // The following code example shows how to implement the copy constructor method. It also implements a static copy factory method that essentially does the same thing as the copy constructor method.
+    // The problem with the copy constructors is their maintenance, i.e., if an object is structually modified, you have to modify the copy constructor.
+    public class CopyObjectConstructor
+    {
+        public string str;
+        public CopyObjectConstructor() {}
+
+        // copy constructor
+        public CopyObjectConstructor(CopyObjectConstructor other)
+        {
+            this.str = other.str;
+        }
+        // copy factory
+        public static CopyObjectConstructor GetInstance(CopyObjectConstructor x)
+        {
+            return new CopyObjectConstructor(x);
+        }
+    }
 }
