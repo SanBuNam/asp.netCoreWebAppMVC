695955080832f3e06632c44b6b7e9d7e151af323
David Lee <david.lee@amwestfunding.com>
2021-09-21 12:27:31 -0700

    shallow copy using MemberwiseClone()

--- asp.netCoreWebAppMVC/General/CopyOfObject.cs
+++ asp.netCoreWebAppMVC/General/CopyOfObject.cs
@@ -7,81 +7,157 @@ using System.Threading.Tasks;
 
 namespace asp.netCoreWebAppMVC.General
 {
-    // Object.MemberwiseClone() method can be used to create a shallow copy of the current object.
-    public class CopyObjectMemberwiseClone
+
+    /*
+     Object.MemberwiseClone Method is used to create a shallow copy or make clone of the current Object. 
+    Shallow copy is a bit-wise copy of an object. In this case, a new object is created and that object has an exact copy of the existing object. 
+    Basically, this method copies the non-static fields of the current object to the new object.
+
+    If a field is a reference type, then the only reference is copied not the referred object. 
+    So here, the cloned object and the original object will refer to the same object.
+    If the field is value type then the bit-by-bit copy of the field will be performed.
+     */
+
+    // syntax : protected object MemberwiseClone ();
+    // Returns : This method returns a Object, which is the shallow copy of existing Object.
+
+    // c# program to clone a object
+    // Using MemberwiseClone() method #1.
+    class GFG1
     {
-        public string str;
+        public int val;
+        public GFG1(int val)
+        {
+            this.val = val;
+        }
+    }
+
+    class GFG2
+    {
+        public GFG1 gg;
+        public GFG2(int val)
+        {
+            // copy the reference of GFG1 to gg
+            this.gg = new GFG1(val);
+        }
+
+        // method for cloning
+        public GFG2 Clone()
+        {
+            // return cloned value using MemberwiseClone() method
+            return ((GFG2)MemberwiseClone());
+        }
+    }
+
+    // Driver code
+    class Geek
+    {
+        // Main method
+        public static void GeekMain()
+        {
+            // object of Class GFG2 with a value 3
+            GFG2 g = new GFG2(3);
+
+            // calling Clone()
+            // "cc" has the reference of Clone()
+            GFG2 cc = g.Clone();
+
+            // accessing the main value
+            Console.WriteLine("Value: " + g.gg.val);
+            // accessing the cloned value
+            Console.WriteLine("cloned value: " + cc.gg.val);
+
+            // set a new value
+            // in variable "val"
+            cc.gg.val = 6;
+
+            // accessing the main value
+            Console.WriteLine("\nValue: " + g.gg.val);
+            // accessing the cloned value
+            Console.WriteLine("cloned value: " + cc.gg.val);
+        
+            /*
+             returns 
+            Value: 3
+            cloned value: 3
+            Value: 6
+            cloned value: 6
+             */
+        }
+    }
+
+
+    // Example2
+    public class GFG : ICloneable
+    {
+        // data members
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        // constructor
+        public GFG(string name, string title, int age)
+        {
+            Name = name;
+            Surname = title;
+            Age = age;
+        }
+
+        // method for cloning
         public object Clone()
         {
-            return this.MemberwiseClone();
+            // return cloned value using MemberwiseClone() method
+            return MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name = {0}, Surname = {1}, Age {2}", Name, Surname, Age);
         }
     }
 
-    public class MemberwiseCloneExample
+    // Driver class
+    public class MainGFGClass
     {
-        public static void RunMain()
+        // Main Method
+        public static void MainMethod()
         {
-            CopyObjectMemberwiseClone obj = new CopyObjectMemberwiseClone();
-            obj.str = "Hello!";
+            GFG g = new GFG("ABC", "XYZ", 26);
 
-            CopyObjectMemberwiseClone copy = (CopyObjectMemberwiseClone)obj.Clone();
-            Console.WriteLine(copy.str);
-        }
-    }
+            // calling Clone() "cg" has reference of Clone()
+            GFG cg = (GFG)g.Clone();
 
-    // Copy Constructor takes another instance of the same class and defines the complier's actions when copying the object.
-    // The copy constructor implementation should perform deep copy for any referenced objects in the class by creating new objects and copying the immutable type's values.
-    // The following code example shows how to implement the copy constructor method. It also implements a static copy factory method that essentially does the same thing as the copy constructor method.
-    // The problem with the copy constructors is their maintenance, i.e., if an object is structually modified, you have to modify the copy constructor.
-    public class CopyObjectConstructor
-    {
-        public string str;
-        public CopyObjectConstructor() {}
+            Console.WriteLine("For old values\nOriginal :");
+            Console.WriteLine(g);
 
-        // copy constructor
-        public CopyObjectConstructor(CopyObjectConstructor other)
-        {
-            this.str = other.str;
-        }
-        // copy factory
-        public static CopyObjectConstructor GetInstance(CopyObjectConstructor x)
-        {
-            return new CopyObjectConstructor(x);
-        }
-    }
+            Console.WriteLine("Cloned :");
+            Console.WriteLine(cg);
 
-    // Deep Clone - The following code example demonstrates how to implement the deep cloning through BinaryFormatter Serialize() and Deserialize() methods.
-    // For this to work, mark your class as serializable through [Serializable].
-    [Serializable]
-    public class CopyObjectDeepClone
-    {
-        public string str;
-    }
+            Console.WriteLine("\nAfter assigning new values");
+            g.Name = "LMN";
+            g.Surname = "QRS";
+            g.Age = 13;
 
-    public static class DeepCopyExtensions
-    {
-        public static T DeepClone<T>(this T obj)
-        {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(stream, obj);
-                stream.Position = 0;
+            Console.WriteLine("Original : ");
+            Console.WriteLine(g);
 
-                return (T)formatter.Deserialize(stream);
-            }
-        }
-    }
+            Console.WriteLine("Cloned : ");
+            // prints the old cloned value
+            Console.WriteLine(cg);
 
-    public class DeepCopyExample
-    {
-        public static void DeepCopyMain()
-        {
-            CopyObjectDeepClone obj = new CopyObjectDeepClone();
-            obj.str = "Hello!";
+            /*
+             For old values
+            Original : 
+            Name = ABC, Surname = XYZ, Age 26
+            Cloned :
+            Name = ABC, Surname = XYZ, Age 26
 
-            CopyObjectDeepClone copy = obj.DeepClone();
-            Console.WriteLine(copy.str);
+            After assigning new values
+            Original : 
+            Name = LMN, Surname = QRS, Age 13
+            Cloned :
+            Name = ABC, Surname = XYZ, Age 26
+             */
         }
     }
 }
